59d25bd2a7112b3a5b5f8f10c8f22123f9e46f83
Dknife <young.min.kang@gmail.com>
2018-12-04 16:03:40 +0900

    t

--- TUScheduler_v0.1/TabBasicInfo.py
+++ TUScheduler_v0.1/TabBasicInfo.py
@@ -8,20 +8,30 @@ class ClassData():
         GradeChoices = ['1', '2', '3', '4']
         XCoord = constant.CLASS_NAME_INDENT + constant.COMBOBOXSIZE + i * constant.CLASSDATASIZE
 
+        self.label1 = wx.StaticText(parent, -1, "과목명", (XCoord,  pos[1]-18))
         self.ClassName = wx.TextCtrl(parent, -1, "", (XCoord,  pos[1]), size=(constant.CLASSNAMESIZE, constant.WIDGET_H))
+        self.label2 = wx.StaticText(parent, -1, "시수", (XCoord + constant.CLASSNAMESIZE, pos[1]-18))
         self.Hours = wx.ComboBox(parent, -1, "", (XCoord + constant.CLASSNAMESIZE, pos[1]), choices=nHoursChoices, size=(constant.COMBOBOXSIZE, constant.WIDGET_H))
+        self.label3 = wx.StaticText(parent, -1, "학년", (XCoord + constant.CLASSNAMESIZE+constant.COMBOBOXSIZE, pos[1]-18))
         self.Grade = wx.ComboBox(parent, -1, "", (XCoord + constant.CLASSNAMESIZE+constant.COMBOBOXSIZE, pos[1]), choices=GradeChoices, size=(constant.COMBOBOXSIZE, constant.WIDGET_H))
+        self.Theory = wx.CheckBox(parent, -1, label="이론수업", pos=(XCoord,  pos[1]+28))
+        self.Multi = wx.CheckBox(parent, -1, label="분반", pos=(XCoord+constant.CLASSNAMESIZE, pos[1] + 28))
 
         #self.ClassName.SetLabel("hello")
     def remove(self):
         self.ClassName.Hide()
         self.Hours.Hide()
         self.Grade.Hide()
+        self.label1.Hide()
+        self.label2.Hide()
+        self.label3.Hide()
+        self.Theory.Hide()
+        self.Multi.Hide()
 
 
 class ProfInfo():
     def __init__(self, parent, i, x, y):
-        message = str(i) + "번째 교수님: "
+        message ="교수"+str(i)+":"
         self.parent = parent
         self.pos = (x,y)
         self.Label = wx.StaticText(parent, -1, message, (x,y))
@@ -34,20 +44,20 @@ class ProfInfo():
         self.ClassArr = []
 
     def OnSelectNClass(self, e):
-        for i in range(self.nClass) :
-            self.ClassArr[i].remove()
 
-        del self.ClassArr
-        self.ClassArr = []
-
-        self.nClass = int(e.GetString())
-
-
-        for i in range(self.nClass):
-            self.ClassArr.append(ClassData(self.parent, i, (self.pos[0] + constant.CLASSGAP, self.pos[1])))
+        nClassInput = int(e.GetString())
+        if nClassInput == self.nClass: return
+        elif nClassInput < self.nClass:
+            for i in range(nClassInput, self.nClass) :
+                self.ClassArr[i].remove()
+            del self.ClassArr[nClassInput:self.nClass]
+            self.nClass = nClassInput
+        elif nClassInput > self.nClass:
+            for i in range(self.nClass, nClassInput):
+                self.ClassArr.append(ClassData(self.parent, i, (self.pos[0] + constant.CLASSGAP, self.pos[1])))
+            self.nClass = nClassInput
 
     def remove(self):
-        print("removing")
         self.Label.Hide()
         self.Name.Hide()
         self.nClassCombo.Hide()
@@ -78,13 +88,7 @@ class TabBasicInfo(wx.Panel):
 
 
         self.labelName = wx.StaticText(self, -1, "성함", (constant.XSTART+constant.PROF_NAME_INDENT, constant.DATA_CATEGORY_LINE))
-        self.labelName = wx.StaticText(self, -1, "과목수", (constant.XSTART+constant.PROF_NCLASS_INDENT, constant.DATA_CATEGORY_LINE))
-
-        for i in range(8) :
-            self.labelName = wx.StaticText(self, -1, "과목명", (constant.XSTART+constant.CLASS_NAME_INDENT+constant.COMBOBOXSIZE+i*constant.CLASSDATASIZE, constant.DATA_CATEGORY_LINE))
-            self.labelName = wx.StaticText(self, -1, "시수", (constant.XSTART+constant.CLASS_NAME_INDENT+constant.COMBOBOXSIZE+constant.CLASSNAMESIZE+i*constant.CLASSDATASIZE, constant.DATA_CATEGORY_LINE))
-            self.labelName = wx.StaticText(self, -1, "학년", (constant.XSTART+constant.CLASS_NAME_INDENT+constant.COMBOBOXSIZE+constant.CLASSNAMESIZE+constant.COMBOBOXSIZE+i*constant.CLASSDATASIZE, constant.DATA_CATEGORY_LINE))
-
+        self.labelName = wx.StaticText(self, -1, "과목수                   * 이론수업은 실습실이 아닌 일반 강의실로 배정해도 무방한 강의입니다.", (constant.XSTART+constant.PROF_NCLASS_INDENT, constant.DATA_CATEGORY_LINE))
 
     def OnClassRoomSelect(self, e):
 
@@ -100,13 +104,12 @@ class TabBasicInfo(wx.Panel):
         elif nProfessorsInput < self.nProfessors :
             for i in range(nProfessorsInput, self.nProfessors) :
                 self.ProfInfoArr[i].remove()
-                print("removing ",i)
             del self.ProfInfoArr[nProfessorsInput:self.nProfessors]
             self.nProfessors = nProfessorsInput
 
         elif nProfessorsInput > self.nProfessors :
             for i in range(self.nProfessors, nProfessorsInput):
-                self.ProfInfoArr.append(ProfInfo(self, i+1, constant.XSTART, 150+i*50))
+                self.ProfInfoArr.append(ProfInfo(self, i+1, constant.XSTART, constant.DATA_CATEGORY_LINE+50+i*constant.PROF_DATA_GAP))
             self.nProfessors = nProfessorsInput
 
     def PrintInfo(self):
